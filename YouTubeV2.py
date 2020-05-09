c7437c9a072dbb088ca46c7be66da999eebaeee9
Malhar Mane <52221408+malharmane9@users.noreply.github.com>
2020-05-09 23:27:57 +0530

    Update YouTubeV2.py

--- YouTubeV2.py
+++ YouTubeV2.py
@@ -1,60 +1,3 @@
-'''import sys, re, operator, collections
-
-class WordFrequenciesModel:
-    """ Models the data. In this case, we're only interested
-    in words and their frequencies as an end result """
-    freqs = {}
-    stopwords = set(open('../stop_words.txt').read().split(','))
-    def __init__(self, path_to_file):
-        self.update(path_to_file)
-
-    def update(self, path_to_file):
-        try:
-            words = re.findall('[a-z]{2,}', open(path_to_file).read().lower())
-            self.freqs = collections.Counter(w for w in words if w not in self.stopwords)
-        except IOError:
-            print("File not found")
-            self.freqs = {}
-
-class WordFrequenciesView:
-    def __init__(self, model):
-        self._model = model
-
-    def render(self):
-        sorted_freqs = sorted(self._model.freqs.items(), key=operator.itemgetter(1), reverse=True)
-        for (w, c) in sorted_freqs[0:25]:
-            print(w, '-', c)
-
-class WordFrequencyController:
-    def __init__(self, model, view):
-        self._model, self._view = model, view
-        view.render()
-
-    def run(self):
-        while True:
-            print("Next file: ") 
-            sys.stdout.flush() 
-            filename = sys.stdin.readline().strip()
-            self._model.update(filename)
-            self._view.render()
-
-
-m = WordFrequenciesModel(sys.argv[1])
-v = WordFrequenciesView(m)
-c = WordFrequencyController(m, v)
-c.run()'''
-
-
-'''This syle of coding is also called MVC. In my code I have used three
-classes of Model, controller and view. this resembles the idea of christa
-lopes code. it has a structural organisation unlike any other. The advantage
-of this type of coding is that more code can be added or existing code can
-be altered without hindering any prevailing process. it is really useful in
-software development. The view only present things to the user controller
-conects the view and the model, the model is where the actual functioning
-happens.'''
-
-
 import urllib.request
 import urllib.parse
 import json
@@ -70,7 +13,7 @@ class Youtubemodel:
         url = 'https://www.googleapis.com/youtube/v3/videos?'
         # The parameters are used for getting the desired result
         parameter = [('key', key), ('part', 'snippet, statistics'),
-                     ('maxResults', '5'), ('chart', 'mostPopular'),
+                     ('maxResults', '10'), ('chart', 'mostPopular'),
                      ('regionCode', 'us')]
         url += urllib.parse.urlencode(parameter)
         # We return the complete url
