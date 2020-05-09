a2b060d2ccf1d0f4e9e25ee17f74e4f88cdc8cdb
Malhar Mane <52221408+malharmane9@users.noreply.github.com>
2020-05-09 23:27:16 +0530

    Update YouTubeV1.py

--- YouTubeV1.py
+++ YouTubeV1.py
@@ -1,100 +1,3 @@
-# Cookbook type of code. By Crista lopes
-'''import sys, string
-
-# The shared mutable data
-data = []
-words = []
-word_freqs = []
-
-#
-# The procedures
-#
-def read_file(path_to_file):
-    """
-    Takes a path to a file and assigns the entire
-    contents of the file to the global variable data
-    """
-    global data
-    with open(path_to_file) as f:
-        data = data + list(f.read())
-
-def filter_chars_and_normalize():
-    """
-    Replaces all nonalphanumeric chars in data with white space
-    """
-    global data
-    for i in range(len(data)):
-        if not data[i].isalnum():
-            data[i] = ' '
-        else:
-            data[i] = data[i].lower()
-
-def scan():
-    """
-    Scans data for words, filling the global variable words
-    """
-    global data
-    global words
-    data_str = ''.join(data)
-    words = words + data_str.split()
-
-def remove_stop_words():
-    global words
-    with open('../stop_words.txt') as f:
-        stop_words = f.read().split(',')
-    # add single-letter words
-    stop_words.extend(list(string.ascii_lowercase))
-    indexes = []
-    for i in range(len(words)):
-        if words[i] in stop_words:
-            indexes.append(i)
-    for i in reversed(indexes):
-        words.pop(i)
-
-def frequencies():
-    """
-    Creates a list of pairs associating
-    words with frequencies
-    """
-    global words
-    global word_freqs
-    for w in words:
-        keys = [wd[0] for wd in word_freqs]
-        if w in keys:
-            word_freqs[keys.index(w)][1] += 1
-        else:
-            word_freqs.append([w, 1])
-
-def sort():
-    """
-    Sorts word_freqs by frequency
-    """
-    global word_freqs
-    word_freqs.sort(key=lambda x: x[1], reverse=True)
-
-
-#
-# The main function
-#
-read_file(sys.argv[1])
-filter_chars_and_normalize()
-scan()
-remove_stop_words()
-frequencies()
-sort()
-
-for tf in word_freqs[0:25]:
-    print(tf[0], ' - ', tf[1])'''
-
-
-'''This code is similar to what I have coded. The cookbook method of
-coding has a procedural processing of code. One function processes only
-one activity. No single function processes two activities. In my example, I
-have separated url making, url opening, data decoding and data printing.
-This is method is given the name cookbook because this code goes in a
-sequence flow just like the sequence flow of ingredients in a cookbook.'''
-
-
 '''I am writing a code for getting the most viewed youtube videos.'''
 import urllib.request
 import urllib.parse
@@ -110,7 +13,7 @@ def make_url():
     url = 'https://www.googleapis.com/youtube/v3/videos?'
     # We set the parameters we want to search
     parameter = [('key', myAPIkey), ('part', 'snippet, statistics'),
-                 ('maxResults', '5'), ('chart', 'mostPopular'),
+                 ('maxResults', '10'), ('chart', 'mostPopular'),
                  ('regionCode', 'us')]
     url += urllib.parse.urlencode(parameter)
     # we call the open_url function to open the url
